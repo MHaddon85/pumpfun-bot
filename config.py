0a74bd32ec9bcdd8445466866e6f470e4e903eae
MHaddon85 <matthewhaddon85@gmail.com>
2025-01-28 04:30:59 -0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    PUMPFUN_API_KEY = "YOUR_PUMPFUN_KEY_HERE"  # From pump.fun
-    TWEETSCOUT_API_KEY = "a9898906-b7d1-4695-991f-cd1bfb7a971c"  # From tweetscout.io
-    TELEGRAM_TOKEN = "7845342999:AAEo8KGQAporoXCiiarTqbrc2I8jZBAwvNQ"  # From @BotFather
-    BONKBOT_API_KEY = "Y2UjE9JfYquaT5sH9Z4EdcVekUymTvN5kpqDNcWZsRkGARo7c6FztKjJCWrM5N2udqyTdBHnGb4uVjiAK8wHhPBhY"        # From bonkbot.io
-    TELEGRAM_CHAT_ID = "@HaddPumpBot"     # Your Telegram channel
